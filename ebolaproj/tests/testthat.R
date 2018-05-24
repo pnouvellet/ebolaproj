b515704ac1638b6a187998a064d40219e3814a9e
pnouvellet <p.nouvellet@imperial.ac.uk>
2018-05-24 12:50:39 +0100

    initial structure
    used https://github.com/reconhub/recontools

---
+++ ebolaproj/tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ebolaproj)
+
+test_check("ebolaproj")
---
+++ test.r
@@ -0,0 +1,3 @@
+devtools::install_github("reconhub/recontools")
+# from within your session
+recontools::init_package("ebolaproj", check_cran_name = FALSE)
